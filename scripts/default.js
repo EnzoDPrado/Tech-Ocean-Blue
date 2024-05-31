26f0b37abbe545ea330a9e025f0142e5f51daee9
Rafael Duarte <rafa.duartef@outlook.com>
2024-05-30 22:03:09 -0300

    Website com responsividade para computadores v1. Sem responsividade para mobile

--- scripts/default.js
+++ scripts/default.js
@@ -21,3 +21,17 @@ function redirectPage(pageRedirect, index = false) {
     } 
     window.location.assign(directory + pageRedirect + '.html');
 }
+
+const header = document.querySelector('header');
+
+// Função para adicionar ou remover a classe .header-scrolled com base no scroll da página
+function handleScroll() {
+    if (window.scrollY > window.innerHeight / 1.2) {
+        header.classList.add('header-scrolled');
+    } else {
+        header.classList.remove('header-scrolled');
+    }
+}
+
+// Adiciona um event listener para o evento scroll
+window.addEventListener('scroll', handleScroll);
